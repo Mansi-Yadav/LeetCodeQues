ef2fe24537e75e2b1f424076c15096ca88edf0c9
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-07-06 14:49:21 +0530

    Time: 10 ms (54.41%), Space: 10.6 MB (71.57%) - LeetHub

---
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    void func(int idx, int target,vector<int>& candidates, vector<int>& ds, vector<vector<int>>& ans ){
+        
+            if(target==0){
+               ans.push_back(ds);
+                return; 
+            }
+        for(int i=idx; i< candidates.size(); i++){
+            if(i>idx && candidates[i]==candidates[i-1])
+                continue;
+            if(candidates[i] > target)
+                break;
+         
+            ds.push_back(candidates[i]);
+            func(i+1, target- candidates[i], candidates, ds, ans);
+            ds.pop_back();
+            
+        }
+    }
+        
+    vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
+     
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> ans;
+        vector<int> ds;
+        func(0,target, candidates, ds, ans);
+       
+        return ans;
+    }
+};
