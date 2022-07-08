8dbb25ede37f66c45f01bcd3f22f34ad56923f8d
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-07-08 23:53:09 +0530

    Time: 2 ms (83.89%), Space: 7.5 MB (73.17%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
-    void findSubset(int idx, vector<int> &nums, vector<int> &ds,  vector<vector<int>> &ans ){
+    void solve(int idx, vector<int>& nums, vector<int>& ds, vector<vector<int>>& ans){
+        
         ans.push_back(ds);
-        for(int i= idx; i<nums.size(); i++){
-            if(i!=idx && nums[i] == nums[i-1])
+        
+        for(int i=idx; i<nums.size(); i++){
+            if(i != idx && nums[i] == nums[i-1])
                 continue;
-            ds.push_back(nums[i]);
-            findSubset(i+1, nums, ds,ans);
-            ds.pop_back();
+                ds.push_back(nums[i]);
+                solve(i+1, nums, ds, ans);
+                ds.pop_back();
+            
         }
         
+        
     }
-    
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
         vector<vector<int>> ans;
         vector<int> ds;
-        sort(nums.begin(), nums.end());
-        findSubset(0, nums, ds, ans);
+        
+        solve(0, nums, ds, ans);
         return ans;
     }
 };
