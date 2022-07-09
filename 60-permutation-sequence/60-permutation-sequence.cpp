bfa74032f750d87015c9a2e6e340fdb7df0a6b4e
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-07-10 00:26:50 +0530

    Time: 4 ms (48.56%), Space: 6 MB (80.53%) - LeetHub

--- 60-permutation-sequence/60-permutation-sequence.cpp
+++ 60-permutation-sequence/60-permutation-sequence.cpp
@@ -1,26 +1,23 @@
 class Solution {
 public:
     string getPermutation(int n, int k) {
-        vector<int> copyNo;
+        vector<int> arr;
         int fact=1;
         for(int i=1; i<n; i++){
+            arr.push_back(i);
             fact= fact*i;
-            copyNo.push_back(i);
         }
-        copyNo.push_back(n);
-        string ans="";
-        k= k-1;
-        
+        arr.push_back(n);
+        k=k-1;
+        string str="";
         while(true){
-            ans= ans+ to_string(copyNo[k/fact]);
-
-            copyNo.erase(copyNo.begin()+ k/fact);
-            if(copyNo.size() == 0)
+            str= str + to_string(arr[k / fact]);
+            arr.erase(arr.begin() +k / fact);
+            if(arr.size() == 0)
                 break;
-            
-            k=k%fact;
-            fact= fact/copyNo.size();
+            k= k%fact;
+            fact= fact/ arr.size();
         }
-        return ans;
+        return str;
     }
 };
