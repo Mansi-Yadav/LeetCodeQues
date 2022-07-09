4b7ce01bf7745b5f4c71397e84e2d70b2eabc3b3
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-07-09 18:46:29 +0530

    Time: 7 ms (75.78%), Space: 10.5 MB (88.82%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,32 +1,28 @@
 class Solution {
 public:
     
-    void func(int idx, int target,vector<int>& candidates, vector<int>& ds, vector<vector<int>>& ans ){
+    void solve(int idx, vector<int>& candidates, int target, vector<vector<int>>& ans, vector<int>& ds){
+        if(target == 0){
+            ans.push_back(ds);
+            return;
+        }
         
-            if(target==0){
-               ans.push_back(ds);
-                return; 
-            }
-        for(int i=idx; i< candidates.size(); i++){
-            if(i>idx && candidates[i]==candidates[i-1])
+        for(int i= idx; i<candidates.size(); i++){
+            if(i>idx && candidates[i] == candidates[i-1])
                 continue;
             if(candidates[i] > target)
                 break;
-         
-            ds.push_back(candidates[i]);
-            func(i+1, target- candidates[i], candidates, ds, ans);
-            ds.pop_back();
             
+            ds.push_back(candidates[i]);
+            solve(i+1, candidates, target- candidates[i], ans, ds);
+            ds.pop_back();
         }
     }
-        
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-     
         sort(candidates.begin(), candidates.end());
         vector<vector<int>> ans;
-        vector<int> ds;
-        func(0,target, candidates, ds, ans);
-       
+         vector<int> ds;
+        solve(0, candidates, target, ans, ds);
         return ans;
     }
 };
