81caac356cea4e44d33566812ba5c10bc042209c
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-07-10 12:59:06 +0530

    Time: 23 ms (26.91%), Space: 11.9 MB (21.51%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -6,43 +6,47 @@ public:
         int row1= row;
         int col1= col;
         while(row >=0 && col >=0){
-            if(board[row][col] =='Q')
+            if(board[row--][col--] =='Q')
                 return false;
-            row--;
-            col--;
         }
         
         row= row1;
         col= col1;
         while(col>=0){
-            if(board[row][col]=='Q')
+            if(board[row][col--]=='Q')
                 return false;
-            col--;
+            //col--;
         }
         
         row= row1;
         col= col1;
         while(row<n && col>=0){
-            if(board[row][col]=='Q')
+            if(board[row++][col--]=='Q')
                 return false;
-            row++;
-            col--;
+            //row++;
+            //col--;
         }
         return true;
     }
     
     
-    void solve(int col,vector<string> board, vector<vector<string>>& ans, int n  ){
+    void solve(int col,vector<string> board, vector<vector<string>>& ans, int n, vector<int>& leftRow, vector<int>& upperDiag, vector<int>& lowerDiag  ){
         if(col== n){
             ans.push_back(board);
             return;
         }
         
         for(int row=0; row<n; row++){
-            if(isSafe(row, col, board, n)){
+            if(leftRow[row] == 0 && upperDiag[row+col] == 0 && lowerDiag[(n-1) + (col-row)] == 0){
                 board[row][col]= 'Q';
-                solve(col+1, board, ans, n);
+                leftRow[row]=1;
+                upperDiag[row+col]=1;
+                lowerDiag[(n-1) + (col-row)]=1;
+                solve(col+1, board, ans, n, leftRow, upperDiag, lowerDiag);
                 board[row][col]= '.';
+                leftRow[row]=0;
+                upperDiag[row+col]=0;
+                lowerDiag[(n-1) + (col-row)]=0;
             }
         }
     }
@@ -53,9 +57,13 @@ public:
         
         string s(n , '.');
         for(int i=0; i<n; i++){
-            board[i]= s;
+            board[i]=s;
         }
-        solve(0, board, ans, n);
+        
+        vector<int> leftRow(n);
+        vector<int> upperDiag(2*n -1), lowerDiag(2*n -1);
+        
+        solve(0, board, ans, n, leftRow, upperDiag, lowerDiag);
         return ans;
     }
 };
