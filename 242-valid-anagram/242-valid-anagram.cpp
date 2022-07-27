d5af51cf9d7fc1f1d65289e0c446207e948f3ef4
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-07-27 22:34:42 +0530

    Time: 8 ms (82.77%), Space: 7.4 MB (45.59%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,20 +1,22 @@
-#include<cstring>
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-       char arr1[s.size()+1];
-        char arr2[t.size()+1];
-        if(s.size() != t.size())
+        int n= s.size();
+        int m= t.size();
+        
+        if(n != m)
             return false;
-       
-        strcpy(arr1, s.c_str());
-        strcpy(arr2, t.c_str());
         
-       std::sort(arr1, arr1+s.size()) ;
-        std::sort(arr2,arr2+t.size());
-        
-        for(int i=0; (i<s.size()|| i<t.size()); i++){
-            if(arr1[i] != arr2[i])
+        int cnt[26]={0};
+        for(int i=0; i<n; i++){
+            cnt[s.at(i)- 'a']++;
+            
+            cnt[t.at(i) - 'a']--;
+            
+        }
+        int si= sizeof(cnt)/sizeof(cnt[0]);
+        for(int i=0; i<si; i++){
+            if(cnt[i] != 0)
                 return false;
         }
         return true;
