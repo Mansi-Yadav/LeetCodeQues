3c9b5fdb515a223a475a9b971f6ac5ee57fe90ab
Mansi Yadav <92424211+Mansi-Yadav@users.noreply.github.com>
2022-09-01 23:18:54 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (71.11%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    
+    void solve(vector<int> &num, vector<int> &ds, int idx, vector<vector<int>> &ans){
+        if(idx == num.size()){
+            ans.push_back(ds);
+            return;
+        }
+        
+        ds.push_back(num[idx]);
+        solve(num, ds, idx+1, ans);
+        ds.pop_back();
+        solve(num, ds, idx+1, ans);
+        
+    }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<vector<int>> ans;
-        
-        int N= nums.size();
-        
-         for(int i=0; i<(1<<N); i++){
-            vector<int> ds;
-            
-            for(int j=0; j<N; j++){
-                if((i&(1<<j))>0)
-                    ds.push_back(nums[j]);
-            }
-             ans.push_back(ds);
-         }
+        vector<int> ds;
+        solve(nums, ds, 0, ans);
         return ans;
     }
 };
